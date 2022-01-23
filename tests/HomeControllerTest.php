72ddca7bde14b0ebee376fb772ab004988d946ef
Frédéric Vanmarcke <fvanmarcke72@gmail.com>
2022-01-23 19:27:46 +0100

    Modification according to codacy

--- tests/HomeControllerTest.php
+++ tests/HomeControllerTest.php
@@ -9,7 +9,7 @@ class HomeControllerTest extends WebTestCase
     public function testSomething(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Hello HomeController!');
