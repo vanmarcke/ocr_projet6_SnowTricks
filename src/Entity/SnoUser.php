0ec15dc36be47bad85b79256940d63ccd96e64ea
Frédéric Vanmarcke <fvanmarcke72@gmail.com>
2022-01-24 12:02:23 +0100

    add email in User entity

--- src/Entity/SnoUser.php
+++ src/Entity/SnoUser.php
@@ -24,6 +24,9 @@ class SnoUser implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string')]
     private $password;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $email;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -92,5 +95,17 @@ class SnoUser implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
     }
 }
