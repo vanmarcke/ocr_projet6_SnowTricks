d9092b75c5551238a771a1aec43d97f5269498c2
Frédéric Vanmarcke <fvanmarcke72@gmail.com>
2022-01-24 18:08:26 +0100

    Configure the disconnection route

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="app_logout")
+     * @Route("/déconnexion", name="app_logout")
      */
     public function logout(): void
     {
