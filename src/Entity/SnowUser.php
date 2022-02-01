57918c5d0081175ce85b0daf144cf4873f0d8968
Frédéric <fvanmarcke72@gmail.com>
2022-02-01 14:16:03 +0100

    Modification according to SymfonyInsight

--- src/Entity/SnowUser.php
+++ src/Entity/SnowUser.php
@@ -103,7 +103,7 @@ class SnowUser implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->plainPassword = null;
     }
 
     public function getEmail(): ?string
