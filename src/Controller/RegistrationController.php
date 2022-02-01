8ce2148691f2317dc132eed67a36d1bbfb234410
Frédéric <fvanmarcke72@gmail.com>
2022-02-01 13:47:40 +0100

    Modifications according to comments

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -63,7 +63,7 @@ class RegistrationController extends AbstractController
             );
             // do anything else you need here, like send an email
 
-            $this->addFlash('success', 'Verifier votre boite mail afin valider votre inscription.');
+            $this->addFlash('success', 'Vérifier votre boite mail afin de valider votre inscription.');
             return $this->redirectToRoute('app_login');
         }
 
