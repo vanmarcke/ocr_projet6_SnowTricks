01b8373e66fc4eb01df519e0ca57ae9bfda0405d
Frédéric <fvanmarcke72@gmail.com>
2022-02-07 11:13:36 +0100

    Add controller function to restore information in the home view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SnowFigureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(SnowFigureRepository $snowFigureRepository): Response
     {
+        //list of published figures, by decreasing id 
+        $figuresPublished = $snowFigureRepository->findBy(['publish' => '1'], ['id' => 'DESC']);
+        $figuresPublishedHome = $snowFigureRepository->findBy(['publish' => '1'], ['id' => 'DESC'], 5, 0);
+        
+        //total number of figures published 
+        $nbFiguresPublished = count($figuresPublished);
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'figures' => $figuresPublishedHome,
+            'nbFigures' => $nbFiguresPublished
         ]);
     }
 }
