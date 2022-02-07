f93202687e1477baf9278fc4889ebb1af3086dd5
Frédéric <fvanmarcke72@gmail.com>
2022-02-07 11:19:00 +0100

    Home routing update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,17 +12,28 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(SnowFigureRepository $snowFigureRepository): Response
     {
-        //list of published figures, by decreasing id 
+        //list of published figures, by decreasing id
         $figuresPublished = $snowFigureRepository->findBy(['publish' => '1'], ['id' => 'DESC']);
         $figuresPublishedHome = $snowFigureRepository->findBy(['publish' => '1'], ['id' => 'DESC'], 5, 0);
-        
-        //total number of figures published 
+
+        //total number of figures published
         $nbFiguresPublished = count($figuresPublished);
-        
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'figures' => $figuresPublishedHome,
-            'nbFigures' => $nbFiguresPublished
+            'nbFigures' => $nbFiguresPublished,
+        ]);
+    }
+
+    #[Route('/blockFigures', name: 'blockFigures', methods: 'GET')]
+    public function blockFigures(SnowFigureRepository $snowFigureRepository): Response
+    {
+        $numFigure = $_GET['numFigure'];
+        $figures = $snowFigureRepository->findBy(['publish' => '1'], ['id' => 'DESC'], 5, $numFigure);
+
+        return $this->render('home/_blockFigures.html.twig', [
+            'figures' => $figures,
         ]);
     }
 }
